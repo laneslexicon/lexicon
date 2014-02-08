c7b0246bd0e6ddf69ddcfdbfc4118b41492fd477
gabanjo <user@example.com>
2014-02-08 08:30:42 +0000

    added != operator

--- place.h
+++ place.h
@@ -45,6 +45,9 @@ class Place {
   bool operator == (const Place & p) {
     return ((m_root == p.m_root) && (m_word == p.m_word) && (m_node == p.m_node));
   }
+  bool operator != (const Place & p) {
+    return ! ((m_root == p.m_root) && (m_word == p.m_word) && (m_node == p.m_node));
+  }
   enum Type { User, History, Bookmark, RestoreTab };
   void setType(int t) { m_type = t;}
   int getType() const { return m_type;}
