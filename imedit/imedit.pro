4a962d3fc236ccbdc14fe495901689139586a6a7
Graeme Andrews <graeme3317@gmail.com>
2014-06-11 10:48:23 +0100

    added testing frameword for imedit/imlineedit

---
+++ imedit/imedit.pro
@@ -0,0 +1,30 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2013-05-05T10:34:31
+#
+#-------------------------------------------------
+
+QT       += core gui printsupport
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = imedit
+TEMPLATE = app
+#LIBS +=   -lboost_thread-mt -lboost_system -lboost_filesystem
+MOC_DIR = ./moc
+OBJECTS_DIR = ./obj
+QMAKE_CXXFLAGS += -Wunused-parameter
+SOURCES += main.cpp\
+	imedit.cpp \
+        imlineedit.cpp \
+	keymap.cpp \
+	inputmapper.cpp \
+        scripts.cpp \
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h \
+	keymap.h \
+	imedit.h \
+        imlineedit.h \
+        scripts.h \
+	inputmapper.h
---
+++ imedit/mainwindow.cpp
@@ -0,0 +1,51 @@
+#include "mainwindow.h"
+#include "imedit.h"
+#include "imlineedit.h"
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent)
+
+{
+  QVBoxLayout * layout = new QVBoxLayout;
+  m_edit = new ImEdit;
+  m_edit->setDebug(true);
+  m_edit->loadMap("buckwalter-1.4.js","Buckwalter");
+  m_lineEdit = new ImLineEdit;
+  m_lineEdit->loadMap("buckwalter-1.4.js","Buckwalter");
+  m_lineEdit->activateMap("Buckwalter");
+  QWidget * w = new QWidget;
+  layout->addWidget(m_lineEdit);
+  layout->addWidget(m_edit);
+  w->setLayout(layout);
+  setCentralWidget(w);
+  createActions();
+  createToolBar();
+  createMenus();
+  createStatusBar();
+}
+
+MainWindow::~MainWindow()
+{
+  qDebug() << "main window destructor";
+}
+void MainWindow::createActions() {
+  m_exitAction = new QAction(tr("Exit"),this);
+  connect(m_exitAction,SIGNAL(triggered()),this,SLOT(on_actionExit()));
+}
+void MainWindow::createToolBar() {
+  m_fileToolBar = addToolBar(tr("&File"));
+  m_fileToolBar->addAction(m_exitAction);
+}
+void MainWindow::createMenus() {
+  m_fileMenu = menuBar()->addMenu(tr("&File"));
+  m_fileMenu->addAction(m_exitAction);
+}
+void MainWindow::createStatusBar() {
+  statusBar()->showMessage(tr("Ready"));
+}
+QSize MainWindow::sizeHint() const {
+  return QSize(800,600);
+}
+void MainWindow::on_actionExit()
+{
+    close();
+}
