6af4fe148cbf9976b32a4b3d88c6cf91b2370af1
Graeme Andrews <graeme3317@gmail.com>
2014-09-17 10:55:41 +0100

    added font options

--- options/rootsoptions.cpp
+++ options/rootsoptions.cpp
@@ -7,12 +7,7 @@ RootsOptions::RootsOptions(QSettings * settings,QWidget * parent) : OptionsWidge
 
   QVBoxLayout * vlayout = new QVBoxLayout;
   QFormLayout * formlayout = new QFormLayout;
-  /*
-  m_collapseAll = new QKeySequenceEdit;
-  m_collapseLetter = new QKeySequenceEdit;
-  m_syncToContents = new QKeySequenceEdit;
-  m_focusToContents = new QKeySequenceEdit;
-  */
+
   m_moveDown = new QLineEdit;
   m_moveDown->setMaxLength(1);
   m_moveUp = new QLineEdit;
@@ -30,7 +25,26 @@ RootsOptions::RootsOptions(QSettings * settings,QWidget * parent) : OptionsWidge
   formlayout->addRow(tr("Single click activates item"),m_singleClick);
   formlayout->addRow(tr("Double click activates item"),m_doubleClick);
 
+  QHBoxLayout * hlayout1 = new QHBoxLayout;
+  m_standardFont = new QLineEdit;
+  m_standardButton = new QPushButton(tr("Click to set font"));
+  hlayout1->addWidget(m_standardFont);
+  hlayout1->addWidget(m_standardButton);
+  formlayout->addRow(tr("Non-Arabic font"),hlayout1);
+
+  QHBoxLayout * hlayout2 = new QHBoxLayout;
+  m_arabicFont = new QLineEdit;
+  m_arabicButton = new QPushButton(tr("Click to set font"));
+  hlayout2->addWidget(m_arabicFont);
+  hlayout2->addWidget(m_arabicButton);
+  formlayout->addRow(tr("Arabic font"),hlayout2);
+
+  connect(m_standardButton,SIGNAL(clicked()),this,SLOT(onSetFont()));
+  connect(m_arabicButton,SIGNAL(clicked()),this,SLOT(onSetFont()));
+
+
   formlayout->addRow(tr("Enable debug"),m_debug);
+
   vlayout->addLayout(formlayout);
 
   m_info = new QLabel;
@@ -62,11 +76,13 @@ void RootsOptions::readSettings() {
   }
   m_settings->beginGroup(m_section);
 
-  m_debug->setChecked(m_settings->value(SID_ROOTS_DEBUG).toBool());
-  m_moveUp->setText(m_settings->value(SID_ROOTS_MOVE_UP).toString());
-  m_moveDown->setText(m_settings->value(SID_ROOTS_MOVE_DOWN).toString());
-  m_expand->setText(m_settings->value(SID_ROOTS_EXPAND).toString());
+  m_debug->setChecked(m_settings->value(SID_CONTENTS_DEBUG).toBool());
+  m_moveUp->setText(m_settings->value(SID_CONTENTS_MOVE_UP).toString());
+  m_moveDown->setText(m_settings->value(SID_CONTENTS_MOVE_DOWN).toString());
+  m_expand->setText(m_settings->value(SID_CONTENTS_EXPAND).toString());
 
+  m_arabicFont->setText(m_settings->value(SID_CONTENTS_ARABIC_FONT).toString());
+  m_standardFont->setText(m_settings->value(SID_CONTENTS_STANDARD_FONT).toString());
   m_settings->endGroup();
 }
 void RootsOptions::writeSettings() {
@@ -75,10 +91,12 @@ void RootsOptions::writeSettings() {
   }
   m_settings->beginGroup(m_section);
 
-  m_settings->setValue(SID_ROOTS_DEBUG,m_debug->isChecked());
-  m_settings->setValue(SID_ROOTS_MOVE_UP,m_moveUp->text());
-  m_settings->setValue(SID_ROOTS_MOVE_DOWN,m_moveDown->text());
-  m_settings->setValue(SID_ROOTS_EXPAND,m_expand->text());
+  m_settings->setValue(SID_CONTENTS_DEBUG,m_debug->isChecked());
+  m_settings->setValue(SID_CONTENTS_MOVE_UP,m_moveUp->text());
+  m_settings->setValue(SID_CONTENTS_MOVE_DOWN,m_moveDown->text());
+  m_settings->setValue(SID_CONTENTS_EXPAND,m_expand->text());
+  m_settings->setValue(SID_CONTENTS_ARABIC_FONT,m_arabicFont->text());
+  m_settings->setValue(SID_CONTENTS_STANDARD_FONT,m_standardFont->text());
   m_settings->sync();
 
   m_settings->endGroup();
@@ -89,18 +107,40 @@ bool RootsOptions::isModified()  {
   m_dirty = false;
 
   m_settings->beginGroup(m_section);
-  v = m_settings->value(SID_ROOTS_DEBUG).toBool();
+  v = m_settings->value(SID_CONTENTS_DEBUG).toBool();
   if (m_debug->isChecked() != v) {
     m_dirty = true;
   }
-  s = m_settings->value(SID_ROOTS_MOVE_UP).toString();
+  s = m_settings->value(SID_CONTENTS_MOVE_UP).toString();
   if (m_moveUp->text() != s) {
     m_dirty = true;
   }
-  s = m_settings->value(SID_ROOTS_MOVE_DOWN).toString();
+  s = m_settings->value(SID_CONTENTS_MOVE_DOWN).toString();
   if (m_moveDown->text() != s) {
     m_dirty = true;
   }
   m_settings->endGroup();
   return m_dirty;
+}
+void RootsOptions::onSetFont() {
+  QPushButton * button = qobject_cast<QPushButton *>(QObject::sender());
+  QFont f;
+  if (button == m_standardButton) {
+    f.fromString(m_standardFont->text());
+  }
+  else {
+    f.fromString(m_arabicFont->text());
+  }
+  bool ok;
+  QFont font = QFontDialog::getFont(&ok, f,  this);
+  if (!ok) {
+    return;
+  }
+  if (button == m_standardButton) {
+    m_standardFont->setText(font.toString());
+  }
+  else {
+    m_arabicFont->setText(font.toString());
+  }
+
 }
--- options/rootsoptions.h
+++ options/rootsoptions.h
@@ -1,6 +1,8 @@
 #ifndef __ROOTSOPTIONS_H__
 #define __ROOTSOPTIONS_H__
 #include "optionswidget.h"
+#include <QFontDialog>
+#include <QPushButton>
 class RootsOptions : public OptionsWidget {
   Q_OBJECT
 
@@ -10,15 +12,19 @@ class RootsOptions : public OptionsWidget {
     void writeSettings();
     void readSettings();
     bool isModified();
+    void onSetFont();
  private:
 
     //    QKeySequenceEdit * m_collapseAll;
     //    QKeySequenceEdit * m_collapseLetter;
     //    QKeySequenceEdit * m_syncToContents;
-    //    QKeySequenceEdit * m_focusToContents;
+    QPushButton * m_standardButton;
+    QPushButton * m_arabicButton;
     QLineEdit * m_moveDown;
     QLineEdit * m_moveUp;
     QLineEdit * m_expand;
+    QLineEdit * m_standardFont;
+    QLineEdit * m_arabicFont;
     QCheckBox * m_singleClick;
     QCheckBox * m_doubleClick;
     QCheckBox * m_debug;
