f3ec19a5a2d12cffdaf7651fd9482ce57af304d7
Graeme Andrews <graeme3317@gmail.com>
2014-10-19 15:39:27 +0100

    QStyledItemDelegate for richtext in a QTableView

---
+++ htmldelegate.cpp
@@ -0,0 +1,34 @@
+#include "htmldelegate.h"
+HtmlDelegate::HtmlDelegate(QObject * parent) : QStyledItemDelegate(parent) {
+  //  qDebug() << Q_FUNC_INFO;
+}
+HtmlDelegate::~HtmlDelegate() {
+  //  qDebug() << __FILE__ << Q_FUNC_INFO;
+}
+void HtmlDelegate::setStyleSheet(const QString & str) {
+  m_stylesheet = str;
+}
+void HtmlDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
+{
+  QTextDocument document;
+  if (m_stylesheet.size() > 0) {
+    document.setDefaultStyleSheet(m_stylesheet);;
+  }
+  //  document.setDocumentMargin(2);
+  QVariant value = index.data(Qt::DisplayRole);
+  if (value.isValid() && !value.isNull()) {
+    QStyleOptionViewItem myOption= option;
+    QTextOption t = document.defaultTextOption();
+    t.setAlignment(Qt::AlignLeft|Qt::AlignVCenter);
+    myOption.displayAlignment = Qt::AlignLeft|Qt::AlignVCenter;
+    document.setDefaultTextOption(t);
+    document.setHtml(value.toString());
+    //QTableWidget * table = qobject_cast<QTableWidget *>(this->parent());
+    QRectF r = myOption.rect;
+    r.setX(0);
+    r.setY(0);
+    painter->translate(myOption.rect.topLeft());
+    document.drawContents(painter,r);
+    painter->translate(-myOption.rect.topLeft());
+  }
+}
