1d30c7aea1cffaeef08536a29b8abdf992e1675a
Graeme Andrews <graeme3317@gmail.com>
2014-10-20 09:08:40 +0100

    Updated to latest version

--- sql/notes_schema.sql
+++ sql/notes_schema.sql
@@ -1,2 +1,2 @@
-CREATE TABLE history(id integer primary key,node text,word text,root text,supplement int,page int,vol int,timewhen text, datasource integer);
-CREATE TABLE notes(id integer primary key,datasource integer,word text,place text,subject text,note text,created text,amended text,data text);
+CREATE TABLE notes(id integer primary key,datasource integer,word text,place text,subject text,note text,created text,amended text,data text, type int);
+CREATE INDEX 'notes_word' on notes (word asc);
