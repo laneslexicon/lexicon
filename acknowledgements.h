30584638b1bc149277c3b4691fcd0e8fa9009ed5
laneslexicon <laneslexicon@theunwalledcity.com>
2015-08-14 10:22:36 +0100

    updated for QsLog

--- acknowledgements.h
+++ acknowledgements.h
@@ -6,7 +6,7 @@ str += "<li><p>The <a href=\"http://www.perseus.tufts.edu/hopper/\">Perseus Digi
 str += "<li><p>The software was built with the opensource edition of the <a href=\"http://qt-project.org\">Qt Framework.</a></p></li>";
 str += "<li><p>The default Arabic font is <a href=\"www.amirifonts.org\">Amiri.</a></p></li>";
 str += "<li><p>The default non-Arabic font <a href=\"https://github.com/mozilla/Fira\">Fira</a> is copyright (c) 2012-2015, The Mozilla Foundation and Telefonica S.A. and is released under the SIL Open Font License, Version 1.1</p></li>";
-str += "<li><p>The logging system, QsLog, is @Ravzan Petru.</p></li>";
+str += "<li><p>The logging system, QsLog, Copyright (c) 2013, Razvan Petru</p></li>";
 str += "<li><p>Sotheby's, for permission to use the <a href=\"http://www.sothebys.com/en/auctions/ecatalogue/2014/arts-islamic-world-l14223/lot.59.html\">image</a> of a printers' manuscript copy of Lane's Lexicon.</p></li>";
 str += "</ul>";
 return str;
