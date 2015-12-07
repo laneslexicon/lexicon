b7ce8b231e4bda3ab3380a99def29010476e511c
laneslexicon <laneslexicon@theunwalledcity.com>
2015-12-07 09:51:26 +0000

    added missing files

---
+++ editpagesetdialog.h
@@ -0,0 +1,63 @@
+#ifndef _EDITPAGESETDIALOG_H_
+#define _EDITPAGESETDIALOG_H_
+#include <QtCore>
+#include <QDialog>
+#include <QDebug>
+#include <QTextEdit>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QPushButton>
+#include <QSettings>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlRecord>
+#include <QSqlError>
+#include <QFile>
+#include <QLabel>
+#include <QSettings>
+#include <QTableWidget>
+#include <QTableWidgetItem>
+#include <QCheckBox>
+#include <QFont>
+#include <QWidget>
+#include <QRegExp>
+#include <QGroupBox>
+#include <QRadioButton>
+#include <QHeaderView>
+#include <QFormLayout>
+#include <QDialogButtonBox>
+#include <QLineEdit>
+#include <QTabWidget>
+#include <QMessageBox>
+#ifdef __APPLE__
+#include <QStyleFactory>
+#endif
+class PageEntryDialog;
+class EditPageSetDialog : public QDialog {
+  Q_OBJECT
+
+ public:
+  EditPageSetDialog(QWidget * parent = 0);
+  ~EditPageSetDialog();
+  QString pageSetTitle() const;
+  int loadCount() const;
+  QList<int> pages() const;
+  static bool deletePageSet(int);
+  private slots:
+    void onSelectPages();
+    void onSelectAll(int);
+    void onApply();
+ private:
+
+    void readSettings();
+    void writeSettings();
+    QMap<int,int> m_pagesets;
+    QPushButton * m_loadButton;
+  int loadTitles();
+  int m_action;
+  QTableWidget * m_setlist;
+  QString m_spanStyle;
+  QMap<int,PageEntryDialog *> m_pageDialogs;
+  //  void createPageDialog(int);
+};
+#endif
